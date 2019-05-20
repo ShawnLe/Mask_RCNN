c020fd9ff206ce4438cafdabb79d69f5f08478b4
letrungson <letrungson1@gmail.com>
2019-05-20 17:22:59 +0800

    some code tests

--- samples/test_sl_mdl.py
+++ samples/test_sl_mdl.py
@@ -17,6 +17,7 @@ import skimage.io
 import matplotlib
 import matplotlib.pyplot as plt
 
+from keras import backend as K
 
 sys.path.append('.')
 sys.path.append('..')
@@ -58,28 +59,57 @@ config.display()
 
 
 # Create model object in inference mode.
-model = modellib.MaskRCNN_test(mode="inference", model_dir=MODEL_DIR, config=config)
+# model = modellib.MaskRCNN_test(mode="inference", model_dir=MODEL_DIR, config=config)
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+for i, lr in enumerate(model.keras_model.layers, 0):
+    print("%d - %s -- %s -- %s -- %s" % (i, lr,lr.name,lr.input_shape, lr.output_shape) )
+
+# ref: http://sujitpal.blogspot.com/2017/10/debugging-keras-networks.html
+def get_outputs(inputs, model):
+    layer_01_fn = K.function([model.layers[0].input, K.learning_phase()], 
+                             [model.layers[1].output]) 
+    layer_23_fn = K.function([model.layers[2].input, K.learning_phase()],
+                             [model.layers[3].output])
+    layer_44_fn = K.function([model.layers[4].input, K.learning_phase()],
+                             [model.layers[4].output])
+    layer_1_out = layer_01_fn([inputs, 1])[0]
+    layer_3_out = layer_23_fn([layer_1_out, 1])[0]
+    layer_4_out = layer_44_fn([layer_3_out, 1])[0]
+    return layer_1_out, layer_3_out, layer_4_out
+
 
 # Load weights trained on MS-COCO
-# print(COCO_MODEL_PATH)
-# model.load_weights(COCO_MODEL_PATH, by_name=True)
+print(COCO_MODEL_PATH)
+model.load_weights(COCO_MODEL_PATH, by_name=True)
 
-# # COCO Class names
-# # Index of the class in the list is its ID. For example, to get ID of
-# # the teddy bear class, use: class_names.index('teddy bear')
-# class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-#                'bus', 'train', 'truck', 'boat', 'traffic light',
-#                'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-#                'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-#                'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-#                'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-#                'kite', 'baseball bat', 'baseball glove', 'skateboard',
-#                'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-#                'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-#                'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-#                'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-#                'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-#                'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-#                'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-#                'teddy bear', 'hair drier', 'toothbrush']
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
 
+# Load a random image from the images folder
+file_names = next(os.walk(IMAGE_DIR))[2]
+image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+
+# Run detection
+# results = model.detect([image], verbose=1)
+
+out_1, out_3, out_4 = get_outputs([image], model.keras_model)
+print("out_1 {}", (out_1))
+print("out_3 {}", (out_3))
+print("out_4 {}", (out_4)) 
