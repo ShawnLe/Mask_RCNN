7a8434f9728f75c2fb8a4a180499892d315c7cac
letrungson <letrungson1@gmail.com>
2019-05-20 17:23:16 +0800

    symlink script added

--- symlink.sh
+++ symlink.sh
@@ -2,4 +2,5 @@
 
 sudo ln -sf /usr/bin/g++-7 /usr/bin/g++
 sudo ln -sf /usr/bin/gcc-7 /usr/bin/gcc
+sudo ln -sf /usr/bin/gcc-7 /usr/bin/x86_64-linux-gnu-gcc
 sudo ln -sf /usr/local/Python-3.6.7/bin/python3.6 /usr/bin/python
